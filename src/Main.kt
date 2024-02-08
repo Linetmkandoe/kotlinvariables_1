3132400b1f976f16f2657dd88d97e5a9f47d92bd
Linetmkandoe <mkandoelinet@gmail.com>
2024-02-08 23:11:56 +0300

    added a file

---
+++ src/Main.kt
@@ -0,0 +1,17 @@
+fun main(){
+    var name: String = "Linet Ndau"
+    name = "Linet Ndau"
+    println(name)
+    var age: Byte = 21
+    age = 21
+    println(age)
+    var kenyan: Boolean = true
+    kenyan = true
+    println(kenyan)
+    var weight = 75
+    weight = 75
+    println(weight)
+    var phoneNumber = "0110941162"
+            phoneNumber = "0110941162"
+    println(phoneNumber)
+}
